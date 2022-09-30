e283341e6d46bcec8e75f7f972a58d63fe93c139
Joshua Kasing Lee <joshualeee2018@gmail.com>
2022-09-30 14:56:36 +1000

    changes to db access

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 #from venv import create
 from website import create_app
+import atexit
+
+import psycopg2
+
+#initialise restaurant class here (constructing it. it should be global)
 
 app = create_app()
 
-#initialise restaurant class here (constructing it. it should be global)
+conn = psycopg2.connect(
+    host="localhost",
+    database="flask_db",
+    user="postgres"
+    )
+
+def closedb():
+    conn.close()
+    
+atexit.register(closedb)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/backend/restaurant.py
+++ website/backend/restaurant.py
@@ -1,8 +1,10 @@
-from init_db import query_db
 from manager import Manager
 from wait_staff import WaitStaff
 from kitchen_staff import KitchenStaff
 from table import Table
+from menu_item import MenuItem
+from main import conn
+from tag import Tag
 class Restaurant:
     def __init__(self, name):
         self.name = name
@@ -16,24 +18,42 @@ class Restaurant:
         
     def populate(self):
         #make for each entry in db, menu items, tables, leaderboard entries, manager, waiter, kitchen
-        result = query_db("select password from staff where role = 'manager'");
+        cur = conn.cursor()
+        cur.execute("select password from staff where role = 'manager'")
+        result = cur.fetchall()
         m_pw = result[0][0]
         new_m = Manager(m_pw, self);
         self.manager = new_m;
         
-        result = query_db("select password from staff where role = 'wait'");
+        cur.execute("select password from staff where role = 'wait'")
+        result = cur.fetchall()
         w_pw = result[0][0]
-        new_w = WaitStaff(w_pw, self);
+        new_w = WaitStaff(w_pw, self)
         self.wait = new_w;
         
-        result = query_db("select password from staff where role = 'kitchen'");
+        cur.execute("select password from staff where role = 'kitchen'")
+        result = cur.fetchall();
         k_pw = result[0][0]
-        new_k = KitchenStaff(k_pw, self);
+        new_k = KitchenStaff(k_pw, self)
         self.kitchen = new_k;
         
-        result = query_db("select num, budget from tables");
+        cur.execute("select num, budget from tables")
+        result = cur.fetchall();
         for tab in result:
             self.tables.append(Table(tab[0], tab[1]))
+            
+        cur.execute("select mi.id, mi.name, mi.cost, mi.category, mi.visible, mi.display_order, mi.description, mi.ingredients, mi.image from menu_item mi")
+        result = cur.fetchall();
+        for item in result:
+            id, name, cost, category, visible, display_order, description, ingredients, image = item; 
+            tag_query = """select t.name from tag t join menu_item_tags mit on mit.tag = t.id where mit.menu_item = %s"""
+            cur.execute(tag_query, [id])
+            tag_list = cur.fetchall()
+            menu_tags = []
+            for name in tag_list:
+                menu_tags.append(Tag(name))
+            
+            self.menu.append(MenuItem(name, cost, category, visible, display_order, description, ingredients, menu_tags, image))
             
         
         
