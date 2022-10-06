5b9fd21d153efe3612577f268b63af010f3f7277
Joshua Kasing Lee <joshualeee2018@gmail.com>
2022-10-06 15:24:22 +1100

    login

--- website/backend/__init__.py
+++ website/backend/__init__.py
@@ -3,6 +3,9 @@ from manager_routes import manager_routes
 from waiter_routes import waiter_routes
 from customer_routes import customer_routes
 from kitchen_routes import kitchen_routes
+from flask import request
+from __init__res import restaurant
+from json import dumps
 
 def create_app():
     app = Flask(__name__)
@@ -13,5 +16,12 @@ def create_app():
     app.register_blueprint(kitchen_routes, url_prefix='/kitchen')
     
     #josh's login route here
-
+    @app.route("/login", methods=['POST'])
+    def user_login():
+        data = request.get_json()
+        role = data["role"]
+        password = password["password"]
+        token = restaurant.login(role, password)
+        res = {"token": token}
+        return dumps(res)
     return app
