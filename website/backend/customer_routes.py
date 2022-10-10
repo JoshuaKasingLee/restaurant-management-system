965be091612ec7b1860621f79e0eadfbf65a344e
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2022-10-10 19:06:17 +1100

    fixed view orders route

--- website/backend/customer_routes.py
+++ website/backend/customer_routes.py
@@ -21,11 +21,17 @@ def choose_table():
 
 @customer_routes.route('/menu', methods=['GET'])
 def display_menu():
+    bearer = request.headers['Authorization']
+    token = bearer.split()[1]
+    restaurant.customer_validate(token)
     res = restaurant.menu_to_JSON(restaurant)
     return res
 
 @customer_routes.route('/order', methods=['POST'])
 def order_dishes():
+    bearer = request.headers['Authorization']
+    token = bearer.split()[1]
+    restaurant.customer_validate(token)
     data = request.get_json()
     table = data["table"]
     menu_item = data["menuItem"]
@@ -33,18 +39,26 @@ def order_dishes():
     for t in restaurant.tables:
         if t.number == table:
             t.order_dishes(menu_item, quantity)
-    return Response(status = 200)
+    return {}
 
-@customer_routes.route('/order?<int:TableNum>', methods=['GET'])
-def view_orders(TableNum):
-    data = request.get_json()
+@customer_routes.route('/order', methods=['GET'])
+def view_orders():
+    bearer = request.headers['Authorization']
+    token = bearer.split()[1]
+    restaurant.customer_validate(token)
+    args = request.args
+    table = args.get("table")
+    orders = {}
     for t in restaurant.tables:
-        if t.number == TableNum:
+        if t.number == int(table):
             orders = t.view_orders()
     return orders
 
 @customer_routes.route('/bill', methods=['POST'])
 def get_bill():
+    bearer = request.headers['Authorization']
+    token = bearer.split()[1]
+    restaurant.customer_validate(token)
     data = request.get_json()
     table = data["table"]
     type = data["type"]
