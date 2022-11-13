86edf33b988ac6280fad81ae0b41ec9671f67b3e
James Pham <jamesphamaus@gmail.com>
2022-11-13 21:55:29 +1100

    beautify customer menu

--- frontend/src/components/customer/Footer.js
+++ frontend/src/components/customer/Footer.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import ListAltRoundedIcon from '@mui/icons-material/ListAltRounded';
@@ -17,15 +18,19 @@ function Footer({initialValue, title}) {
   };
 
   return (
-    <Box sx={{ position: 'fixed', bottom: 10 }}>
-      {title !== 'Table Selection' && <Box width="160px" alignItems='end' sx={{ mx: '10px', bgcolor: 'text.secondary', borderRadius: '16px' }}>
-        <Typography color='background.paper' align="center" variant="h4" component="div" sx={{ flexGrow: 1 }}>
-        Table {localStorage.getItem('table')}
-        </Typography>
-      </Box>} 
-      {title !== 'Table Selection' && title !== 'Budget Allocation' && title !== 'Bill'
-      ? (<BottomNavigation  
-        sx={{ position: 'fixed', display:'flex', justifyContent:'space-between', bottom: 0, left: 180, right: 0 }}  
+    <Card sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      position: 'fixed',
+      bottom: 20,
+      left: '50%',
+      ml: '-120px',
+      width: '240px',
+      height: '60px',
+      padding: 0}}
+    >
+      <BottomNavigation
         elevation={3} 
         value={value} 
         onChange={handleChange}
@@ -33,42 +38,23 @@ function Footer({initialValue, title}) {
         <BottomNavigationAction
           label="Menu"
           value="menu"
-          icon={<RestaurantMenuRoundedIcon fontSize="large" />}
+          icon={<RestaurantMenuRoundedIcon fontSize="medium" />}
           component={Link} to={'/customer/menu'}
         />
         <BottomNavigationAction
             label="Order"
             value="order"
-            icon={<ListAltRoundedIcon fontSize="large" />}
+            icon={<ListAltRoundedIcon fontSize="medium" />}
             component={Link} to={'/customer/order'}
         />
         <BottomNavigationAction
             label="Game" 
             value="game" 
-            icon={<SportsEsportsRoundedIcon fontSize="large" />} 
+            icon={<SportsEsportsRoundedIcon fontSize="medium" />} 
             component={Link} to={'/customer/game'}
         />
-        <BottomNavigationAction 
-          label="Admin" 
-          value="admin" 
-          icon={<AdminPanelSettingsRoundedIcon fontSize="large" />} 
-          component={Link} to={'/'}
-        />
-      </BottomNavigation>)
-      : (<BottomNavigation 
-        sx={{ position: 'fixed', display:'flex', justifyContent:'flex-end', bottom: 0, left: 180, right: 0 }}  
-        elevation={3} 
-        value={value} 
-        onChange={handleChange}
-      >
-        <BottomNavigationAction 
-          label="Admin" 
-          value="admin" 
-          icon={<AdminPanelSettingsRoundedIcon fontSize="large" />} 
-          component={Link} to={'/'}
-        />
-      </BottomNavigation>)}
-    </Box>
+      </BottomNavigation>
+    </Card>
   );
 }
 
--- frontend/src/theme/typography.js
+++ frontend/src/theme/typography.js
@@ -29,7 +29,7 @@ const typography = {
     fontWeight: 700,
     lineHeight: 80 / 64,
     fontSize: pxToRem(40),
-    ...responsiveFontSizes({ sm: 52, md: 58, lg: 64 }),
+    ...responsiveFontSizes({ sm: 58, md: 64, lg: 70 }),
   },
   h2: {
     fontWeight: 700,
